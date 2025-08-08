e3151c9949b67370ed1d26c94c0d56f33a9c5d86
hezretpirliyev13 <122713004+hezretpirliyev13@users.noreply.github.com>
2025-08-08 16:32:45 +0500

    Update Timer1_normal_mode.ino

--- Timer1_normal_mode.ino
+++ Timer1_normal_mode.ino
@@ -1,24 +1,24 @@
 volatile bool timer = false;
 void setup() {
   Serial.begin(115200);
+  pinMode(9, OUTPUT);
+  cli();
   TCCR1A = 0;
   TCCR1B = 0;
   TCCR1C = 0;
   TCNT1 = 0;
-  TCCR1B |= (1 << CS11);  //Prescaler = 8 and period is 0.5us(microseconds) and Normal mode
+  TCCR1A |= (1 << COM1A0);               //Enable OC1A toggle mode
+  TCCR1B |= (1 << CS11) | (1 << WGM12);  //Prescaler = 8 and period is 0.5us(microseconds) and Normal mode
 
   TIMSK1 |= (1 << OCIE1A);  //0.5us * 65 535 = 0.032s = 32ms
-  OCR1A = 9;            //5us period
-  // TIFR1 |= (1 << OCF1A);  //Enable the Output Compare Flag register
+  OCR1A = 9;                //10us period
+
+  // TIFR1 |= (1 << OCF1A);  //Enable the Output Compare Flag register manually
+  sei();
 }
 
 void loop() {
-  if (timer) {
-    Serial.println(micros() / 1000);
-    timer = false;
-  }
 }
 ISR(TIMER1_COMPA_vect) {
-  TCNT1=0;
   timer = true;
 }
