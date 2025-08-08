79c73440c2d24a6962b425e72309b011b3c2ea68
hezretpirliyev13 <122713004+hezretpirliyev13@users.noreply.github.com>
2025-08-08 16:48:25 +0500

    Update timer1_ctc_mode.ino

--- timer1_CTC_mode/timer1_ctc_mode.ino
+++ timer1_CTC_mode/timer1_ctc_mode.ino
@@ -1,24 +1,24 @@
 volatile bool timer = false;
 void setup() {
-  Serial.begin(115200);
-  pinMode(9, OUTPUT);
-  cli();
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCCR1C = 0;
-  TCNT1 = 0;
-  TCCR1A |= (1 << COM1A0);               //Enable OC1A (D9 pin) toggle mode
-  TCCR1B |= (1 << CS11) | (1 << WGM12);  //Prescaler = 8 and period is 0.5us(microseconds) and Normal mode
+    Serial.begin(115200);
+    pinMode(9, OUTPUT);
+    cli();
+    TCCR1A = 0;
+    TCCR1B = 0;
+    TCCR1C = 0;
+    TCNT1 = 0;
+    TCCR1A |= (1 << COM1A0);               //Enable OC1A (D9 pin) toggle mode
+    TCCR1B |= (1 << CS11) | (1 << WGM12);  //Prescaler = 8 and period is 0.5us(microseconds) and CTC mode
 
-  TIMSK1 |= (1 << OCIE1A);  //0.5us * 65 535 = 0.032s = 32ms
-  OCR1A = 9;                //10us period
+    TIMSK1 |= (1 << OCIE1A); //Enable Output Compare A Match Interrupt Enable register
+    OCR1A = 9;                //10us period
 
   // TIFR1 |= (1 << OCF1A);  //Enable the Output Compare Flag register
-  sei();
+    sei();
 }
 
 void loop() {
 }
 ISR(TIMER1_COMPA_vect) {
-  timer = true;
+    timer = true;
 }
